f1086c9d3d4cdec0a199ee9b3b2c72796ca2a854
davidhintonlawrence <dlawrence2016@kellogg.northwestern.edu>
2015-05-21 17:45:27 -0500

    Model validations
    Require entries for tasks and category create

--- swoosh/app/models/category.rb
+++ swoosh/app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ActiveRecord::Base
 
   has_many :tasks
 
+  # require category name
+  validates :name, :presence => true
+
+
 end
--- swoosh/app/models/task.rb
+++ swoosh/app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ActiveRecord::Base
 
   belongs_to :category
 
+  # require description in task
+  validates :description, :presence => true
+
 end
